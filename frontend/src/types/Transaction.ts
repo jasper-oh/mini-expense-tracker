12a396bafe4947385b1763bc50b7dbae32a338b3
Jasper Oh <ohyj0906@gmail.com>
2025-08-25 16:07:40 -0700

    [FE-28] create pinia store for transaction, category, fix AddTransaction

--- frontend/src/types/Transaction.ts
+++ frontend/src/types/Transaction.ts
@@ -9,3 +9,13 @@ export interface Transaction {
     createdAt: string;
     updatedAt: string;
 }
+
+export interface CreateTransactionData {
+    amount: number;
+    currency: string;
+    date: string;
+    description: string;
+    category_id: number;
+}
+
+export interface CategoryBalance {}
